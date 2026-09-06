46d419c81373217ced9d801e9db02a0b5896f550
Thirupathi Selvaraj V R <thirupathiselvaraj0506@gmail.com>
2026-09-06 18:38:37 +0530

    Time: 0 ms (100%), Space: 8.3 MB (57.04%) - LeetHub

--- 0306-additive-number/0306-additive-number.cpp
+++ 0306-additive-number/0306-additive-number.cpp
@@ -6,7 +6,7 @@ public:
         for(int flen = 1; flen <= N/2; flen++){
             if(num[0] == '0' && flen > 1) break;
             for(int slen = 1; N-flen-slen >= max(flen, slen); slen++){              //thirdnumlen=N-flen-slen
-                if(num[flen] == '0' && slen >1) break;
+                if(num[flen] == '0' && slen > 1) break;
                 ll fnum = stoll(num.substr(0, flen));
                 ll snum = stoll(num.substr(flen, slen));
                 string bal = num.substr(flen+slen);
